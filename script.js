55d7fad4f7547cbb846adcdef1d350bc03d852c5
kawenka <silvestrekewan@gmail.com>
2026-02-20 16:45:30 +0100

    added morpion test

---
+++ script.js
@@ -0,0 +1,63 @@
+const statusDisplay = document.querySelector('#status');
+const cells = document.querySelectorAll('.cell');
+let gameActive = true;
+let currentPlayer = "X";
+let gameState = ["", "", "", "", "", "", "", "", ""];
+
+const winningConditions = [
+    [0, 1, 2], [3, 4, 5], [6, 7, 8], // Lignes
+    [0, 3, 6], [1, 4, 7], [2, 5, 8], // Colonnes
+    [0, 4, 8], [2, 4, 6]             // Diagonales
+];
+
+function handleCellClick(clickedCellEvent) {
+    const clickedCell = clickedCellEvent.target;
+    const clickedCellIndex = parseInt(clickedCell.getAttribute('data-index'));
+
+    if (gameState[clickedCellIndex] !== "" || !gameActive) return;
+
+    gameState[clickedCellIndex] = currentPlayer;
+    clickedCell.innerText = currentPlayer;
+    
+    checkResult();
+}
+
+function checkResult() {
+    let roundWon = false;
+    for (let i = 0; i <= 7; i++) {
+        const winCondition = winningConditions[i];
+        let a = gameState[winCondition[0]];
+        let b = gameState[winCondition[1]];
+        let c = gameState[winCondition[2]];
+        if (a === '' || b === '' || c === '') continue;
+        if (a === b && b === c) {
+            roundWon = true;
+            break;
+        }
+    }
+
+    if (roundWon) {
+        statusDisplay.innerText = `Le joueur ${currentPlayer} a gagné !`;
+        gameActive = false;
+        return;
+    }
+
+    let roundDraw = !gameState.includes("");
+    if (roundDraw) {
+        statusDisplay.innerText = "Match nul !";
+        gameActive = false;
+        return;
+    }
+
+    currentPlayer = currentPlayer === "X" ? "O" : "X";
+    statusDisplay.innerText = `C'est au tour de ${currentPlayer}`;
+}
+
+document.querySelectorAll('.cell').forEach(cell => cell.addEventListener('click', handleCellClick));
+document.querySelector('#reset').addEventListener('click', () => {
+    gameActive = true;
+    currentPlayer = "X";
+    gameState = ["", "", "", "", "", "", "", "", ""];
+    statusDisplay.innerText = "C'est au tour de X";
+    cells.forEach(cell => cell.innerText = "");
+});
